565a187879c20e9e9e6a2715f9b6028a22f710e6
Alex <pryzalex@gmail.com>
2020-10-18 15:53:56 +0300

    some more

--- jsAdv6/Validate.js
+++ jsAdv6/Validate.js
@@ -1,5 +1,5 @@
-﻿
-// регистрация обработчиков событий элементов формы.
+﻿window.addEventListener("load", init, false);
+
 function init() {
     form1.userName.onchange = nameOnChange;
     form1.email.onchange = emailOnChange;
@@ -7,18 +7,16 @@ function init() {
     form1.onsubmit = onsubmitHandler;
 }
 
-// метод проверки значения в элементе по регулярному выражению.
+
 function validate(elem, pattern) {
     var res = pattern.test(elem.value);
     if (res === false) {
         elem.className = "invalid";
-    } // установка CSS класса 
-    else {
+    } else {
         elem.className = "valid";
     }
 }
 
-// обработчики событий изменения текста в окне.
 function nameOnChange() {
     var pattern = /\S/;
     validate(this, pattern);
@@ -34,28 +32,16 @@ function zipcodeOnChange() {
     validate(this, pattern);
 }
 
-// событие при отправке формы на сервер.
 function onsubmitHandler(event) {
-
-    for (var i = 0; i < form1.elements.length; ++i) {
-        if (form1.elements[i].type === "text")
-            form1.elements[i].className = "valid";
-    }
-
     var invalid = false;
-
     for (var i = 0; i < form1.elements.length; ++i) {
-        var elem = form1.elements[i];
-        // проверка типа элемента и наличия обработчика события onchange.
-        if (elem.type == "text" && elem.onchange) {
-            elem.onchange(); // запуск события onchange
-            if (elem.className == "invalid") invalid = true;
+        if (form1.elements[i].className == "invalid") {
+            invalid = true;
         }
     }
 
     if (invalid) {
         alert("Допущены ошибки при заполнении формы.");
         event.preventDefault();
-        return false; // отмена отправки формы.
     }
 }
