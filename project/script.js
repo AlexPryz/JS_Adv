b1d6c692cc469bc0978db74a5b2f2293fb34d04c
Alex <pryzalex@gmail.com>
2020-11-29 23:39:27 +0200

    v3

--- project/script.js
+++ project/script.js
@@ -1,104 +1,128 @@
-var requestURL = 'http://swapi.dev/api/people/?page=1';
 var x;
 var infoCard = document.querySelector('#card');
+var requestURL = 'http://swapi.dev/api/people/?page=1';
+var init = {
+    method: 'GET'
+};
 
-letsGo();
+letsGo(requestURL);
 
-function letsGo() {
-    var divInCont = document.querySelector('.icons-container');
-    var request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
-    request.onload = function () {
-        var jsonObj = request.response;
-        createDivs(jsonObj);
-        x = jsonObj;
-        var upArrow = document.querySelector('#up');
-        upArrow.addEventListener("click", up);
-        var downArrow = document.querySelector('#down');
-        downArrow.addEventListener("click", down);
-        var infoCard = document.querySelector('#back');
-        infoCard.addEventListener("click", close);
-    };
+function letsGo(requestURL) {
+    $(document.querySelector('info')).fadeIn("slow");
 
-    function createDivs(jsonObj) {
-        divInCont.innerHTML = " ";
-        for (var i = 0; i < jsonObj.results.length; i++) {
-            var newDiv = document.createElement('div');
-            newDiv.innerHTML = jsonObj.results[i].name;
-            newDiv.id = i;
-            newDiv.addEventListener("click", go);
-            divInCont.appendChild(newDiv);
+    fetch(requestURL, init)
+        .then(function (resp) {
+            return resp.json();
+        }).then(function (data) {
+            var divInCont = document.querySelector('.icons-container');
+            var jsonObj = data;
+            createDivs(jsonObj);
+            x = jsonObj;
+            if (!x.previous) {
+                document.querySelector('#up-arrow').style = "border: solid grey; border-width: 0 7px 7px 0;display: inline-block; padding: 7px;";
+            } else {
+                document.querySelector('#up-arrow').style = "border: solid white; border-width: 0 7px 7px 0;display: inline-block; padding: 7px;";
 
-        }
-    }
+            }
+            if (!x.next) {
+                document.querySelector('#down-arrow').style = "border: solid grey; border-width: 0 7px 7px 0;display: inline-block; padding: 7px;";
 
-    function go() {
-        $(infoCard).hide();
-        var jsonObj = request.response;
-        $(infoCard).fadeToggle("200");
-        var idStr = this.id;
-        var header = document.querySelector('#header');
-        header.innerHTML = jsonObj.results[idStr].name;
-        var year = document.querySelector('#year');
-        year.innerHTML = jsonObj.results[idStr].birth_year;
-        var gender = document.querySelector('#gender');
-        gender.innerHTML = jsonObj.results[idStr].gender;
+            } else {
+                document.querySelector('#down-arrow').style = "border: solid white; border-width: 0 7px 7px 0;display: inline-block; padding: 7px;";
 
-        function getPlanet(url) {
-            var request = new XMLHttpRequest();
-            request.open('GET', url);
-            request.responseType = 'json';
-            request.send();
-            request.onload = function () {
-                var jsonObj = request.response;
-                
+            }
+after
+            var upArrow = document.querySelector('#up');
+            upArrow.addEventListener("click", up);
+            var downArrow = document.querySelector('#down');
+            downArrow.addEventListener("click", down);
+            var infoCard = document.querySelector('#back');
+            infoCard.addEventListener("click", close);
 
-                var planet = document.querySelector('#planet');
-                planet.innerHTML = jsonObj.name;
-            };
-        }
-        getPlanet(jsonObj.results[idStr].homeworld);
+            function createDivs(jsonObj) {
+                divInCont.innerHTML = " ";
+                for (var i = 0; i < jsonObj.results.length; i++) {
+                    var newDiv = document.createElement('div');
+                    newDiv.innerHTML = jsonObj.results[i].name;
+                    newDiv.id = i;
+                    newDiv.addEventListener("click", go);
+                    divInCont.appendChild(newDiv);
 
-        function getSubtype(url) {
-            var request = new XMLHttpRequest();
-            request.open('GET', url);
-            request.responseType = 'json';
-            request.send();
-            var subtype = document.querySelector('#subtype');
-            subtype.innerHTML = "its not assigned";
-            request.onload = function () {
-                var jsonObj = request.response;
-                if (!jsonObj) {
-                    return;
-                } else {
-                    subtype = document.querySelector('#subtype');
-                    subtype.innerHTML = jsonObj.name;
                 }
 
-            };
-        }
-        getSubtype(jsonObj.results[idStr].species);
-
-        var url = jsonObj.results[idStr].films;
-        for (var j = 0; j < url.length; j++) {
-            getMovies(jsonObj.results[idStr].films);
-            function getMovies(url) {
-               
-                var request = new XMLHttpRequest();
-                request.open('GET', url[j]);
-                request.responseType = 'json';
-                request.send();
-                request.onload = function () {
-                    var jsonObj = request.response;
-                    var movies = document.querySelector('#movies');
-                    movies.innerHTML += jsonObj.title + "<br>";
-                };
             }
-            movies.innerHTML = " ";
-        }
-    }
+
+
+            function go() {
+                document.querySelector('#card').style.display = "none";
+
+
+                var header = document.querySelector('#header');
+                header.innerHTML = x.results[this.id].name;
+
+                var year = document.querySelector('#year');
+                year.innerHTML = x.results[this.id].birth_year;
+                var gender = document.querySelector('#gender');
+                gender.innerHTML = x.results[this.id].gender;
+
+                fetch(x.results[this.id].homeworld, {
+                        method: 'GET'
+                    })
+                    .then(function (resp) {
+                        return resp.json();
+                    }).then(function (data) {
+                        var planet = document.querySelector('#planet');
+                        planet.innerHTML = data.name;
+                    }).catch(function (err) {
+                        console.error(err)
+                    });
+
+                var subtype = document.querySelector('#subtype');
+
+
+                if (!x.results[this.id].species[0]) {
+                    subtype.innerHTML = "n/a";;
+                } else {
+                    fetch(x.results[this.id].species[0], {
+                            method: 'GET'
+                        })
+                        .then(function (resp) {
+                            return resp.json();
+                        }).then(function (data) {
+                            subtype = document.querySelector('#subtype');
+                            subtype.innerHTML = data.name;
+
+                        }).catch(function (err) {
+                            console.error(err);
+                        });
+                }
+
+
+                for (var j = 0; j < x.results[this.id].films.length; j++) {
+                    getMovies(x.results[this.id].films[j]);
+
+                    function getMovies(url) {
+
+                        fetch(url, init)
+                            .then(function (resp) {
+                                return resp.json();
+                            }).then(function (data) {
+                                var movies = document.querySelector('#movies');
+                                movies.innerHTML += data.title + "<br>";
+
+
+                            }).catch(function (err) {
+                                console.error(err)
+                            });
+                    }
+                    movies.innerHTML = " ";
+                }
+                $(document.querySelector('#card')).fadeIn("slow");
+            }
+
+        }).catch(function (err) {
+            console.error(err)
+        });
 }
 
 function up() {
@@ -106,7 +130,7 @@ function up() {
         return;
     } else {
         requestURL = x.previous;
-        letsGo();
+        letsGo(requestURL);
     }
 }
 
@@ -115,10 +139,10 @@ function down() {
         return;
     } else {
         requestURL = x.next;
-        letsGo();
+        letsGo(requestURL);
     }
 }
 
 function close() {
-    $(infoCard).fadeToggle("slow");
+    $(document.querySelector('#card')).fadeOut("slow");
 }
